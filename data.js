8b2943ae151ec15df43a9b1dd9701536286dd35c
jstgrowup <deysubham999@gmail.com>
2022-06-15 23:14:51 +0530

    updates

---
+++ data.js
@@ -0,0 +1,49 @@
+let data = [{
+    image: 'https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/7/277cf77missc00000081_1.jpg',
+    title: 'Miss Claire Eyelash Glue - Clear Tone',
+    weight: '(50gm)',
+    quantity: 1,
+    id: 'BBJ0003398',
+    strikedOffPrice: 350.00,
+    price: 322.00,
+    discount: '8% Off',
+    brand: 'Miss Claire',
+    discription: `Go long and strong in your lash-game with Miss Claire Eyelash Glue. Let’s get all your falsies in their place to slay all day long. This gentle eyelash adhesive is latex free, non-toxic and pH-balanced for safe use. It is waterproof and holds your eyelashes securely in place making them look natural and flawless. It features a handy bottle with an easy-to-use applicator and a formula that blends in seamlessly. Clear tone allows seamless bond between extension and natural lashes.
+                MRP : 350
+                Best Before : JAN 2024
+                About the Brand: Presenting an entire colorful range of face, lips and eye cosmetics that enhance your beauty and amplify your daily make-up looks. Loved by makeup fans across India, Miss Claire cosmetics are of premier quality, trendy, affordable and are formulated using the finest ingredients that care for your skin. The products are manufactured in accordance with European Standards. Choose from a huge range of Miss Claire makeup and beauty products right from lipstick, bronzers, highlighters, foundation, eyeliners to concealers all available in highly pigmented formula and textures. Miss Claire makeup products are affordably priced, long lasting and suit all skin tones.
+                Origin : Made In South Korea
+                Name of Mfg / Importer / Brand: Eureka Cosmo Pvt. Ltd. Address of Mfg / Importer / Brand: Eureka Cosmo Pvt. Ltd. 13, Gokul Regency-Ii, Thakur Complex, Kandivali (E), Mumbai -400 101.`,
+    productCategory: 'Eyes',
+    skinType: 'All Skin Type',
+    shades: 'Peach Rose',
+    multipleImages: ['https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/2/7/277cf77missc00000081_1.jpg', 'https://www.beautybebo.com/pub/media/catalog/produ…ae888adf23d57cf627ad/5/1/51ccwnye-ks._sl1094_.jpg'],
+    howToUse: `Step 1: Dot a thin layer of glue straight from the applicator along the band of the false lash from corner to corner.
+    Step 2: Wait 30 seconds for the adhesive to become sticky.
+    Step 3: Adhere the strip to your lash line and gently press it in place.
+    Step 4: Ensure the cap is secure post every use to avoid the glue from drying out`
+}, {
+    image: 'https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/3/6/3699c478906105612174-2.jpg',
+    title: 'Maybelline New York Fit Me Primer - Dewy+Smooth',
+    weight: '(30ml)',
+    quantity: 1,
+    id: 'BBJ0003397',
+    strikedOffPrice: 549.00,
+    price: 521.00,
+    discount: '5% off',
+    brand: 'Maybelline',
+    discription: `Go long and strong in your lash-game with Miss Claire Eyelash Glue. Let’s get all your falsies in their place to slay all day long. This gentle eyelash adhesive is latex free, non-toxic and pH-balanced for safe use. It is waterproof and holds your eyelashes securely in place making them look natural and flawless. It features a handy bottle with an easy-to-use applicator and a formula that blends in seamlessly. Clear tone allows seamless bond between extension and natural lashes.
+                MRP : 350
+                Best Before : JAN 2024
+                About the Brand: Presenting an entire colorful range of face, lips and eye cosmetics that enhance your beauty and amplify your daily make-up looks. Loved by makeup fans across India, Miss Claire cosmetics are of premier quality, trendy, affordable and are formulated using the finest ingredients that care for your skin. The products are manufactured in accordance with European Standards. Choose from a huge range of Miss Claire makeup and beauty products right from lipstick, bronzers, highlighters, foundation, eyeliners to concealers all available in highly pigmented formula and textures. Miss Claire makeup products are affordably priced, long lasting and suit all skin tones.
+                Origin : Made In South Korea
+                Name of Mfg / Importer / Brand: Eureka Cosmo Pvt. Ltd. Address of Mfg / Importer / Brand: Eureka Cosmo Pvt. Ltd. 13, Gokul Regency-Ii, Thakur Complex, Kandivali (E), Mumbai -400 101.`,
+    productCategory: 'Eyes',
+    skinType: 'All Skin Type',
+    shades: 'Peach Rose',
+    multipleImages: ['https://www.beautybebo.com/pub/media/catalog/product/cache/a19d5a96b74d37bf18297d1e71af48ed/5/b/5b990e16902395773146_3.jpg', 'https://www.beautybebo.com/pub/media/catalog/product/cache/a19d5a96b74d37bf18297d1e71af48ed/5/b/5b990e16902395773146_2.jpg', 'https://www.beautybebo.com/pub/media/catalog/product/cache/a19d5a96b74d37bf18297d1e71af48ed/5/b/5b990e16902395773146_4.jpg'],
+    howToUse: `Step 1: Dot a thin layer of glue straight from the applicator along the band of the false lash from corner to corner.
+    Step 2: Wait 30 seconds for the adhesive to become sticky.
+    Step 3: Adhere the strip to your lash line and gently press it in place.
+    Step 4: Ensure the cap is secure post every use to avoid the glue from drying out.`}]
+   localStorage.setItem("cartData", JSON.stringify(data))
---
+++ script.js
@@ -0,0 +1,110 @@
+let click = document.querySelector("#click")
+let lists = document.querySelector(".list")
+click.addEventListener("click", function () {
+    if (lists.classList.contains("links")) {
+        lists.classList.remove("links")
+    }
+    else {
+        lists.classList.add("links")
+    }
+
+})
+let cartData = JSON.parse(localStorage.getItem("cartData")) || []
+let display = (data) => {
+    document.getElementById("main").innerHTML = ""
+    data.map((el,i) => {
+        let cards = document.createElement("div")
+        cards.setAttribute("id", "cards")
+        let all = document.createElement("div")
+        all.setAttribute("id", "all")
+        let imgS = document.createElement("div")
+        imgS.setAttribute("id", "imgS")
+        let img = document.createElement("img")
+        img.src = el.image
+        imgS.append(img)
+        let sec = document.createElement("div")
+        sec.setAttribute("id", "second")
+        let title = document.createElement("h3")
+        title.setAttribute("id", "title")
+        title.innerHTML = el.title
+        sec.append(title)
+        let third = document.createElement("div")
+        third.setAttribute("id", "third")
+        let price = document.createElement("div")
+        price.setAttribute("id", "price")
+        price.innerHTML = `Rs ` + el.price
+
+        let qaty = document.createElement("div")
+        qaty.setAttribute("id", "qty")
+
+
+        let qty = document.createElement("select")
+        qty.innerHTML = `<option value="none">Qty:</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6<span>Max Qty</span></option>`
+        qty.addEventListener("change", function () {
+            qtychange(el, qty)
+        })
+     
+        qaty.append(qty)
+        
+        let tot = document.createElement("div")
+        tot.setAttribute("id", "tot")
+        let pricep = document.createElement("p")
+        pricep.innerHTML = `Rs`+(el.price*el.quantity).toFixed(2)
+        tot.append(pricep)
+        third.append(price, qaty, tot)
+        all.append(imgS, sec, third)
+        let btns = document.createElement("div")
+        btns.setAttribute("id", "btns")
+        let edit = document.createElement("div")
+        edit.setAttribute("id", "edit")
+        let p1 = document.createElement("p")
+        p1.innerHTML = "Edit"
+        edit.append(p1)
+        let remove = document.createElement("div")
+        remove.setAttribute("id", "remove")
+        remove.addEventListener("click", () => {
+            deletefun(el, i)
+        })
+        let p2 = document.createElement("p")
+
+        p2.innerHTML = "Remove"
+        remove.append(p2)
+        btns.append(edit, remove)
+        cards.append(all, btns)
+        document.getElementById("main").append(cards)
+    })
+}
+display(cartData)
+
+function deletefun(el, i) {
+    // console.log("clicked");
+    cartData.splice(i, 1)
+    localStorage.setItem("cartData", JSON.stringify(cartData))
+    window.location.reload()
+}
+function qtychange(el, qty) {
+    el.quantity = qty.value
+    localStorage.setItem("cartData", JSON.stringify(cartData))
+    cartData = JSON.parse(localStorage.getItem("cartData")) || []
+
+    display(cartData)
+    window.location.reload()
+
+}
+let total = cartData.reduce(function (acc, el) {
+    return acc + (el.price * el.quantity)
+}, 0)
+console.log(total);
+// total = total.toFixed(2)
+localStorage.setItem("totalamt", total)
+// document.getElementById("tot").innerHTML = total
+let sunidhi=(Number(total) + 55.0).toFixed(2)
+document.getElementById("total").innerHTML = total
+document.getElementById("wholeTot").innerHTML = sunidhi
+localStorage.setItem("sunidhi",sunidhi)
